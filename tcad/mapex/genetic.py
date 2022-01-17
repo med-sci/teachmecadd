19e6c21be2431afcd6088fdc98d8468aeb209c4b
Yury Kashkur <lupus_lucis@mail.ru>
2022-01-17 15:30:26 +0300

    Add a  genetic algorithm based pharmacophore builder

---
+++ tcad/mapex/genetic.py
@@ -0,0 +1,213 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdMolDescriptors, SDWriter
+import random
+from loguru import logger
+from statistics import stdev
+from statistics import mean
+from rdkit import RDLogger
+
+lg = RDLogger.logger()
+lg.setLevel(RDLogger.CRITICAL) 
+
+
+class GA:
+    def __init__(
+        self,
+        smiles,  n_conformers, n_inds,
+        mutation_chance,
+        generations,
+        crippen=False,
+        verbose = True,
+        ):
+        self._smiles = smiles
+        self._nconfs = n_conformers
+        self._n_inds = n_inds
+        self._mut_chance = mutation_chance
+        self._num_gens = generations
+        self._useCrippen = crippen
+        self._cache = []
+        self.population = []
+        self.best_chromosome = None
+        self.verbose = verbose
+
+    def _set_up(self):
+        self._mols = self._create_confs()
+        self._ref = self._mols[0]
+        self._prbs = self._mols[1:]
+        self._MMFFs = self._MMFF_props
+        self._Crippens = self._crippen_contribs
+        self._population = self.__population
+
+
+    def _create_confs(self):
+        mols = []
+        logger.info("Generating conformations..",)
+
+        for smile in self._smiles:
+            mol = Chem.MolFromSmiles(smile)
+            mol_hs = Chem.AddHs(mol)
+            _ = AllChem.EmbedMultipleConfs(mol_hs, self._nconfs)
+            mols.append(Chem.RemoveHs(mol_hs))
+
+        logger.info("Done..")
+        return mols
+
+    class _Chromosome:
+        def __init__(self, n_confs, n_genes):
+            self._n_confs = n_confs
+            self._n_genes = n_genes
+            self.score = None
+            self.chromosome = self._chromosome
+
+        def __repr__(self):
+            return "chromosome"
+
+        @property
+        def _chromosome(self):
+            chromosome = [
+                random.randint(0, self._n_confs - 1) for _ in range(self._n_genes)
+            ]
+            return chromosome
+
+    def _cache_population(self, population):
+        for chromosome in population:
+
+            if chromosome.chromosome not in self._cache:
+                self._cache.append(chromosome.chromosome)
+        return self._cache
+
+    @property
+    def _MMFF_props(self):
+        return [AllChem.MMFFGetMoleculeProperties(m) for m in self._mols]
+
+    @property
+    def _crippen_contribs(self):
+        return [rdMolDescriptors._CalcCrippenContribs(m) for m in self._mols]
+
+    @property
+    def __population(self):
+        return [
+            self._Chromosome(self._mols[0].GetNumConformers(), len(self._mols))
+            for _ in range(self._n_inds)
+        ]
+
+    def __fitness(self, chromosome):
+        if chromosome.chromosome in self._cache:
+            return chromosome
+        else:
+            ref = self._ref
+            prbs = self._prbs
+            scores = []
+
+            for i in range(len(prbs)):
+
+                if self._useCrippen:
+                    o3a = AllChem.GetCrippenO3A(
+                        prbs[i],
+                        ref,
+                        self._crippen_contribs[i + 1],
+                        self._crippen_contribs[0],
+                        prbCid=chromosome.chromosome[i + 1],
+                        refCid=chromosome.chromosome[0],
+                    )
+                else:
+                    o3a = AllChem.GetO3A(
+                        prbs[i],
+                        ref,
+                        self._MMFFs[i + 1],
+                        self._MMFFs[0],
+                        prbCid=chromosome.chromosome[i + 1],
+                        refCid=chromosome.chromosome[0],
+                    )
+                rmsd = o3a.Align()
+                matches = o3a.Matches()
+                score = (len(matches) / ref.GetNumAtoms()) * 100 + min(
+                    (rmsd / 0.04) * 100, 100
+                )
+                scores.append(score)
+            total = round((mean(scores) - stdev(scores)), 2)
+            chromosome.score = total
+            return chromosome
+
+    def __order_population(self, population):
+        return sorted(population, key=lambda chromosome: chromosome.score, reverse=True)
+
+    def __crossover(self, population):
+        splt = random.randint(1, len(population[0].chromosome) - 1)
+
+        if len(population) % 2 == 0:
+            len_pop = len(population) - 1
+        else:
+            len_pop = len(population)
+
+        for i in range(1, len_pop, 2):
+            children1 = (
+                population[i].chromosome[0:splt] + population[i + 1].chromosome[splt:]
+            )
+            children2 = (
+                population[i + 1].chromosome[0:splt] + population[i].chromosome[splt:]
+            )
+            population[i].chromosome = children1
+            population[i + 1].chromosome = children2
+        return population
+
+    def __mutation(self, chromosome, chance):
+        assert chance > 0 and chance <= 1
+        distribution = [chance, 1 - chance]
+        random_number = random.choices([1, 0], distribution)
+
+        if random_number[0] == 1:
+            gene = random.randint(0, len(chromosome.chromosome) - 1)
+            chromosome.chromosome[gene] = random.randint(0, chromosome._n_confs - 1)
+        return chromosome
+
+    def run(self, logg_iter):
+        self._set_up()
+
+        population = self._population
+
+        for gen in range(self._num_gens):
+            population = [self.__fitness(chromosome) for chromosome in population]
+            population = self.__order_population(population)
+            self._cache_population(population)
+
+            if self.verbose and gen % logg_iter==0:
+                logger.info(f'Best chromosome score is: {population[0].score}')
+
+            population = self.__crossover(population)
+            population = [population[0]] + [
+                self.__mutation(chromosome, self._mut_chance)
+
+                for chromosome in population[1:]
+            ]
+        population = [self.__fitness(chromosome) for chromosome in population]
+        population = self.__order_population(population)
+
+        if self.verbose:
+            logger.info(f'Best chromosome score is: {population[0].score}')
+        self.population = population
+        self.best_chromosome = population[0].chromosome
+
+    def get_molecules(self):
+        ref = self._ref
+        prbs = self._prbs
+        chromosome = self.best_chromosome
+
+        for i, mol in enumerate(prbs):
+            o3a = AllChem.GetO3A(
+                mol,
+                ref,
+                self._MMFFs[i+1],
+                self._MMFFs[0],
+                prbCid=chromosome[i + 1],
+                refCid=chromosome[0],
+            )
+            o3a.Align()
+        mols = [ref] + prbs
+        return mols
+
+    def write(self, molecules, chromosome, path):
+        with Chem.SDWriter(path) as writer:
+
+            for molecule, conformation in zip(molecules, chromosome):
+                writer.write(molecule, confId=conformation)
---
+++ tcad/mapex/pharmacophore.py
@@ -0,0 +1,145 @@
+from sklearn import cluster
+from rdkit.Chem import ChemicalFeatures
+import math
+import numpy as np
+import os
+from rdkit import RDConfig
+import py3Dmol
+
+
+
+class Pharmacophore:
+    def __init__(self, mol, gene):
+        self._fdef = os.path.join(RDConfig.RDDataDir,'BaseFeatures.fdef')
+        self._mol = mol
+        self._gene = gene
+        self._factory = ChemicalFeatures.BuildFeatureFactory(self._fdef)
+        self.acceptors, self.donors, self.hydrophobics = self._features
+
+    @property
+    def _features(self):
+        mol = self._mol
+        acceptors = [
+            list(f.GetPos(confId = self._gene))
+            for f in self._factory.GetFeaturesForMol(mol, includeOnly="Acceptor", confId=self._gene)
+        ]
+        donors = [
+            list(f.GetPos(confId = self._gene))
+            for f in self._factory.GetFeaturesForMol(mol, includeOnly="Donor", confId=self._gene)
+        ]
+        hydrophobics = [
+            list(f.GetPos(confId = self._gene))
+            for f in self._factory.GetFeaturesForMol(mol, includeOnly="Hydrophobe", confId=self._gene)
+        ]
+        return acceptors, donors, hydrophobics
+
+
+class PharmComplex:
+    def __init__(self, molecules, chromosome):
+        self.molecules = molecules
+        self.chromosome = chromosome
+        self._p_list = [Pharmacophore(m, g) for m, g in zip(self.molecules, self.chromosome)]
+        self._feat_dict = self._features
+        self._coords = {}
+
+    def get_coords(self):
+        return self._coords
+
+    @property
+    def _features(self):
+        accs = []
+        dons = []
+        hyds = []
+
+        for pharmacophore in self._p_list:
+            accs.extend(pharmacophore.acceptors)
+            dons.extend(pharmacophore.donors)
+            hyds.extend(pharmacophore.hydrophobics)
+        feat_dict = {"Acceptors": accs, "Donors": dons, "Hydrophobics": hyds}
+        return feat_dict
+
+    @property
+    def _num_mols(self):
+        return math.ceil(len(self._p_list) * 0.75)
+
+    def _cluster_feats(self, feats, eps=1, min_samples=4):
+        dbscan = cluster.DBSCAN(eps, min_samples=min_samples)
+        dbscan.fit(feats)
+        lbls = dbscan.labels_
+        n_clus = len(np.unique(dbscan.labels_)) - 1
+        clus_dict = {}
+
+        for k in range(n_clus):
+            clus_dict[k] = []
+
+        for i in range(len(feats)):
+
+            for j in range(n_clus):
+
+                if lbls[i] == j:
+                    clus_dict[j].append(feats[i])
+        return clus_dict
+
+    def _get_centers(self, clus_dict):
+        centr_list = []
+
+        for k in clus_dict.keys():
+            x, y, z = (0, 0, 0)
+
+            for i in clus_dict[k]:
+                x = x + i[0]
+                y = y + i[1]
+                z = z + i[2]
+            den = len(clus_dict[k])
+            centr_list.append(
+                [round((x / den), 4), round((y / den), 4), round((z / den), 4)]
+            )
+        return centr_list
+
+    def create(self, distance=1, n_features=None):
+
+        if n_features:
+            num_mols = n_features
+        else:
+            num_mols = self._num_mols
+
+        feat_dict = self._feat_dict
+        coords = {}
+
+        for k in feat_dict.keys():
+            clus_dict = self._cluster_feats(feat_dict[k], distance, num_mols)
+            centr_list = self._get_centers(clus_dict)
+            coords[k] = centr_list
+
+        self._coords = coords
+
+    def show(self, mol_blocks, coords):
+        xyzview = py3Dmol.view(width=1000,height=400)
+
+        for mol_block in mol_blocks:
+            xyzview.addModel(mol_block)
+
+        xyzview.setStyle({'stick':{}})
+        xyzview.setBackgroundColor('0xeeeeee')
+
+        for key, value in coords.items():
+                radius = 1.25
+                color = "purple"
+                label = "Acceptor"
+
+                if key == "Donors":
+                    radius = 1
+                    color ="blue"
+                    label = "Donor"
+
+                if key == "Hydrophobics":
+                    radius = 0.75
+                    color ="gray"
+                    label = "Hydrophobic"
+
+                for coord_set in value:
+                    center = {'x':coord_set[0],'y':coord_set[1],'z':coord_set[2]}
+                    xyzview.addSphere({'radius':radius, 'color':color, 'center':center, "wireframe":True})
+                    xyzview.addLabel(label, {"position": center, "backgroundColor": color, "backgroundOpacity":0.8})
+
+        return xyzview
